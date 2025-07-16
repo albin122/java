7e627067d3c61a342e16bcfac750d4302996cff3
albin122 <albinphilipgigi@gmail.com>
2025-07-16 10:44:24 +0530

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,10 @@
+package pg;
+import java.util.Scanner;
+public class userinput {
+public static void main(String [] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number:");
+	int number=sc.nextInt();
+	System.out.println("The number is:"+number);
+}
+}
