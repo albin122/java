ce436cb3f6208ef6a526162829cb754add0c2e1b
albin122 <albinphilipgigi@gmail.com>
2025-07-23 09:43:26 +0530

    Add files via upload

---
+++ oddnum.java
@@ -0,0 +1,14 @@
+package prg1;
+
+public class oddnum {
+	public static void main(String[]args) {
+		 System.out.println("Odd numbers from 1 to 50:");
+		 for (int i = 1; i <= 50; i++) {
+	            if (i % 2 == 0) {
+	                continue; 
+	            }
+	            System.out.println(i+"") ;   
+	}
+
+}
+}
