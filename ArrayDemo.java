f25041e35f05d291d52c6388711c45f42e18b961
albin122 <albinphilipgigi@gmail.com>
2025-08-13 09:49:22 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,18 @@
+package prg1;
+
+import java.util.Scanner;
+
+public class ArrayDemo {
+public static void main(String[] args) {
+	int [] array = new int[10];
+	Scanner sc =new Scanner(System.in);
+	System.out.println("Enter the elements ");
+	for(int i=0;i<10;i++) {
+		array[i]=sc.nextInt();
+	}
+	
+	for(int num:array) {
+		System.out.println("elements are"+num+"\t");
+	}
+}
+}
